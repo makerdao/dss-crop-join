89044be91ad34b74f10aa2048d509ddce4bee4fd
Brian McMichael <brian@brianmcmichael.com>
2021-04-13 18:29:52 -0400

    Add some documentation, cleanup vars and interfaces

--- src/sushi.sol
+++ src/sushi.sol
@@ -16,30 +16,36 @@ interface MasterChefLike {
 contract SushiJoin is CropJoin {
 
     // --- Auth ---
-    mapping (address => uint) public wards;
+    mapping (address => uint256) public wards;
     function rely(address usr) external auth {
         wards[usr] = 1;
-
         emit Rely(usr);
     }
     function deny(address usr) external auth {
         wards[usr] = 0;
-
         emit Deny(usr);
     }
     modifier auth {
-        require(wards[msg.sender] == 1, "GemJoin/not-authorized");
+        require(wards[msg.sender] == 1, "SushiJoin/not-authorized");
         _;
     }
 
     MasterChefLike  immutable public masterchef;
     uint256         immutable public pid;
-    bool            public live = true;
+    bool                      public live;
 
     // --- Events ---
     event Rely(address indexed usr);
     event Deny(address indexed usr);
 
+    /**
+        @param vat_         MCD_VAT DSS core accounting module
+        @param ilk_         Collateral type
+        @param gem_         The collateral LP token address
+        @param bonus_       The SUSHI token contract address.
+        @param masterchef_  The SushiSwap MCV1 contract address.
+        @param pid_         The index of the sushi pool.
+    */
     constructor(address vat_, bytes32 ilk_, address gem_, address bonus_, address masterchef_, uint256 pid_)
         public
         CropJoin(vat_, ilk_, gem_, bonus_)
@@ -54,6 +60,7 @@ contract SushiJoin is CropJoin {
 
         ERC20(gem_).approve(masterchef_, uint256(-1));
         wards[msg.sender] = 1;
+        live = true;
     }
     function nav() public override returns (uint256) {
         return total;
