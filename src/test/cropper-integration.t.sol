b195fc67ccc93d7c7bb5f7a6855612ad3184de91
Kurt Barry <kurt.m.barry@gmail.com>
2021-06-07 21:58:10 -0400

    more progress on test setup

--- src/test/cropper-integration.t.sol
+++ src/test/cropper-integration.t.sol
@@ -24,36 +24,75 @@ import "./token.sol";
 import "../crop.sol";
 import "../cropper.sol";
 
+contract Pip {
+    uint256 public val;
+    function set(uint256 val_) external {
+        val = val_;
+    }
+    function peek() external returns (bytes32, bool) {
+        return (bytes32(val), true);
+    }
+}
+
+contract Abacus is Pip {
+    function price(uint256, uint256) external view returns (uint256) {
+        return val;
+    }
+}
+
 contract CropperIntegrationTest is TestBase {
     Token gem;
     Token bonus;
     CropJoin join;
     CropClipper cropper;
+    Pip pip;
+    Abacus abacus;
     bytes32 constant ILK = "GEM-A";
 
-    VatAbstract vat;
-    DogAbstract dog;
+    VatAbstract  vat;
+    DogAbstract  dog;
+    SpotAbstract spotter;
 
     function setUp() public {
-        vat = VatAbstract(0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B);
-        dog = DogAbstract(0x135954d155898D42C90D2a57824C690e0c7BEf1B);
+        vat     =  VatAbstract(0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B);
+        dog     =  DogAbstract(0x135954d155898D42C90D2a57824C690e0c7BEf1B);
+        spotter = SpotAbstract(0x65C79fcB50Ca1594B025960e539eD7A9a6D434A3);
 
         // Give this contract admin access on the vat
-        hevm.store(
-            address(vat),
-            keccak256(abi.encode(address(this), uint256(0))),
-            bytes32(uint256(1))
-        );
+        giveAuthAccess(address(vat), address(this));
         assertEq(vat.wards(address(this)), 1);
 
         // Initialize GEM-A in the Vat
         vat.init(ILK);
+
+        // Give this contract admin access on the spotter
+        giveAuthAccess(address(spotter), address(this));
+        assertEq(spotter.wards(address(this)), 1);
+
+        // Initialize price feed
+        pip = new Pip();
+        pip.set(WAD);  // Initial price of $1 per gem
+        spotter.file(ILK, "pip", address(pip));
+        spotter.file(ILK, "mat", 15 * RAY / 10);  // 150% collateralization ratio
+        spotter.poke(ILK);
+
+        gem     = new Token(18, 10**6 * WAD);
+        bonus   = new Token(18, 10**6 * WAD);
+        join    = new CropJoin(address(vat), ILK, address(gem), address(bonus));
+        cropper = new CropClipper(address(vat), address(spotter), address(dog), address(join));
+        cropper.rely(address(dog));
+
+        // Set up pricing
+        Abacus abacus = new Abacus();
+        abacus.set(pip.val());
+        cropper.file("calc", address(abacus));
     }
 
     function test_kick() public {
 //        cropper.kick(tab, lot, usr, address(this));
     }
 
+    function test_bark() public {}
     function test_take_all() public {}
     function test_take_return_collateral() public {}
     function test_take_multiple_calls() public {}
